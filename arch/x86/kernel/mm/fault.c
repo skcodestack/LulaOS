7b703410bbbc06ff3bf8f4534f7a54be1000f81c
shikai <kai.shi@wbstar.com>
2026-08-07 15:49:43 +0800

    fix: fix other error

--- arch/x86/kernel/mm/fault.c
+++ arch/x86/kernel/mm/fault.c
@@ -5,13 +5,12 @@
 
 asmlinkage void do_page_fault(struct pt_regs *regs, unsigned long error_code)
 {
-    unsigned long * p = NULL;
 	unsigned long cr2 = 0;
 	//cr2 保存 发生缺页异常错误的线性地址
 	__asm__	__volatile__("movl	%%cr2,	%0":"=r"(cr2)::"memory");
 
-	p = (unsigned long *)(regs->esp + 0x98);
-	printk("do_page_fault(14),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n",error_code , regs->esp , *p);
+	printk("do_page_fault(14),ERROR_CODE:%#018lx,EIP:%#018lx,CS:%#06lx,EFLAGS:%#010lx\n",
+		error_code, regs->eip, regs->cs & 0xffff, regs->eflags);
 
 	if(!(error_code & 0x01))
 		printk("Page Not-Present,\t");
